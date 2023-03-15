2cb4efb89045a418b24f1b2c8ce9fc2b6f1d7b68
tekerau666 <tekerau666@mail.ru>
2023-03-15 13:24:25 +0400

    add github icon in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IoLogoVk, IoLogoWhatsapp,  } from 'react-icons/io';
+import {IoLogoGithub, IoLogoVk, IoLogoWhatsapp,} from 'react-icons/io';
 
 function Footer() {
     return (
@@ -7,7 +7,7 @@ function Footer() {
             <div className="social-icons" style={{marginLeft:"50px"}}>
                 <a href="https://www.vk.com/" style={{color:"white"}} target={"_blank"}><IoLogoVk size={"2em"}/></a>
                 <a href="https://www.whatsapp.com/" style={{color:"#43d854", marginLeft:"10px", marginTop:"10px"}} target={"_blank"}><IoLogoWhatsapp size={"2em"} /></a>
-                <a href="https://www.instagram.com/"></a>
+                <a href="https://github.com/tekerau666/online-store-full" style={{color:"#1d1a1a", marginLeft:"10px", marginTop:"10px"}} target={"_blank"}><IoLogoGithub size={"2em"}/></a>
             </div>
             <div className="creator" style={{display: "flex", justifyContent: "center", color:"white"}}>
                 [Напишите че нибудь сюда]
